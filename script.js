699674714b01cab5f10c06c17262c6324b32b0ae
kaxa-bekov <kaxa.bekov@gmail.com>
2023-11-09 06:59:46 -0500

    Worked on sub-info display

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+// Main info elements
 const conditionsExpandButton = document.getElementById("expand-conditions");
 const hourlyAndConditionsWrapper = document.getElementById("hourlyAndConditionsId");
 const extendedConditionsWrapper =  document.getElementById("extendedConditionsWrapper");
+// Sub info elements
+const dailyForecast = document.getElementById("dailyForecast");
+const subHourlyForecast = document.getElementById("subHourlyForecast");
+const dailyForecastUL = document.querySelector(".daily-forecast-div > ul");
 
 conditionsExpandButton.addEventListener('click',() =>{
 
     hourlyAndConditionsWrapper.style.display = 'none';
     extendedConditionsWrapper.style.display = 'grid';
+    subWeatherDisplayChange();
 
 })
+
+function subWeatherDisplayChange(){
+    dailyForecast.style.flexBasis = "66%";
+    dailyForecast.style.padding = "25px";
+    dailyForecastUL.style.gap = "13px"
+    subHourlyForecast.style.display = "block";
+
+}
